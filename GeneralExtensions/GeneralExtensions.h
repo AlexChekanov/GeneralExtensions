a87caba0d751a62a522cd013655b3ae9aea95bcb
Alexey Chekanov <chekanov@hotmail.com>
2018-03-05 16:47:30 -0500

    Start

---
+++ GeneralExtensions/GeneralExtensions.h
@@ -0,0 +1,19 @@
+//
+//  GeneralExtensions.h
+//  GeneralExtensions
+//
+//  Created by Alexey Chekanov on 3/5/18.
+//  Copyright © 2018 Tips & Tricks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeneralExtensions.
+FOUNDATION_EXPORT double GeneralExtensionsVersionNumber;
+
+//! Project version string for GeneralExtensions.
+FOUNDATION_EXPORT const unsigned char GeneralExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeneralExtensions/PublicHeader.h>
+
+
